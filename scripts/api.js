1481f14a6f52db670b70f1f9ec5f3bc8ec6498fe
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-10-10 20:37:17 +0300

    first commit

---
+++ scripts/api.js
@@ -0,0 +1,47 @@
+const url = 'https://shazam.p.rapidapi.com/search?term=ceza&locale=tr&offset=0&limit=10';
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'd3e1b88b5amsh5d3244362967668p117b86jsn38a0d42d8561',
+        'x-rapidapi-host': 'shazam.p.rapidapi.com'
+    }
+};
+
+
+export default class API {
+    async getPopular() {
+        const data1 = await this.searchMusic("Linkin Park");
+        const data2 = await this.searchMusic("eminem");
+        return [...data1, ...data2];
+    }
+
+    async searchMusic(query) {
+        const url = `https://shazam.p.rapidapi.com/search?term=${query}&locale=tr`;
+
+        const res = await fetch(url, options);
+        const data = await res.json();
+
+        const formatted = data.tracks.hits.map((item) => item.track);
+        return formatted;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/ui.js
@@ -0,0 +1,91 @@
+export default class UI {
+    constructor() {
+        this.list = document.querySelector(".list");
+        this.form = document.querySelector("form");
+        this.title = document.querySelector("#title");
+        this.player = document.querySelector(".player");
+        this.audio = document.querySelector(".player audio");
+    }
+
+    renderCards(songs) {
+        this.list.innerHTML = "";
+
+        songs.forEach((song) => {
+            const div = document.createElement("div");
+            div.className = "card";
+            div.dataset.title = song.title;
+            div.dataset.subtitle = song.subtitle;
+            div.dataset.img = song.images.coverarthq;
+            div.dataset.mp3 = song.hub.actions[1].uri;
+
+            div.innerHTML = `
+           <figure>
+              <img
+                src="${song.images.coverarthq}"
+              />
+              <div class="play">
+                <i class="bi bi-play-fill"></i>
+              </div>
+            </figure>
+
+          <div class="card-info">
+            <h3>${song.title}</h3>
+            <h4>${song.subtitle}</h4>
+          </div>
+      `;
+            this.list.appendChild(div);
+        });
+    }
+
+
+    renderLoader() {
+        this.list.innerHTML = `
+<div class="loader">
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+</div>
+
+    `;
+    }
+
+    updateTitle(text) {
+        this.title.textContent = text;
+    }
+
+    renderPlayer(song) {
+        this.player.innerHTML = `
+      <div class="info">
+        <img
+          src="${song.img}"
+        />
+
+        <div>
+          <h5>${song.title}</h5>
+          <p>${song.subtitle}</p>
+        </div>
+      </div>
+
+      <audio controls autoplay>
+        <source src="${song.mp3}" />
+      </audio>
+
+      <div class="icons">
+        <i class="bi bi-music-note-list"></i>
+        <i class="bi bi-boombox-fill"></i>
+        <i class="bi bi-pc-display"></i>
+      </div>
+    `;
+
+        const audio = this.player.querySelector("audio");
+
+        audio.addEventListener("play", this.toggleAnimation);
+        audio.addEventListener("pause", this.toggleAnimation);
+    }
+
+    toggleAnimation() {
+        const img = document.querySelector(".info img");
+        img.classList.toggle("animate");
+    }
+}
